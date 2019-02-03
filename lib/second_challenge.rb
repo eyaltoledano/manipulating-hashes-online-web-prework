6a7b016bf7af36818dc2899b54eb1a5df345b00c
Eyal Toledano <eyaltoledano@me.com>
2019-02-03 19:56:43 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,10 +7,5 @@ def second_challenge
   }
 
   #code your solution here!
-  grocery_list = []
-
-  groceries.each do |key, value|
-    grocery_list << key[value]
-  end
-  grocery_list
+  groceries.values.flatten
 end
